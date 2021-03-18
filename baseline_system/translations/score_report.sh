cb107df7b95327a67be5200704a707d290ed327a
abteen <abteen.ebrahimi@gmail.com>
2021-03-18 13:34:51 -0600

    update score report

--- baseline_system/translations/score_report.sh
+++ baseline_system/translations/score_report.sh
@@ -1,7 +1,60 @@
-for lang in aym bzd hch nah quy tar
-do
-echo ${lang}
+#!/bin/bash
 
-python ../../evaluate.py --system_output ${lang}_es/${lang}_es.bpe.hyp --gold_reference ${lang}_es/${lang}_es.bpe.ref
+lang_name=../../data/ashaninka-spanish
+lang=cni
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
 
-done
+lang_name=../../data/aymara-spanish
+lang=aym
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/bribri-spanish
+lang=bzd
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/guarani-spanish
+lang=gn
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/hñähñu-spanish
+lang=oto
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/nahuatl-spanish
+lang=nah
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/quechua-spanish
+lang=quy
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/raramuri-spanish
+lang=tar
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/shipibo_konibo-spanish
+lang=shp
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
+echo -----------------------------------
+
+lang_name=../../data/wixarika-spanish
+lang=hch
+echo $lang_name
+python ../../evaluate.py --system_output ${lang}_es/${lang}_es.hyp --gold_reference ${lang_name}/dev.${lang}
